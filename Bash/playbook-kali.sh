bc95100c5984e36025becca23c8f97b2a358362a
Sk3pper <13051136+Sk3pper@users.noreply.github.com>
2023-11-05 23:28:56 +0100

    golang installation works

--- Bash/playbook-kali.sh
+++ Bash/playbook-kali.sh
@@ -20,7 +20,7 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 usage() {
 msg "${NOFORMAT}"
 cat << EOF # remove the space between << and EOF, this is due to web plugin issue
-Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [--all] [--vscode] [--zsh] [--zsh-user] [--pl10k] [--pyenv] [--docker] [--golang] -s stable-debian-version -u zsh-user
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [--all] [--vscode] [--zsh] [--user] [--pl10k] [--pyenv] [--docker] [--golang] -s stable-debian-version -u zsh-user
 
 Available options:
 -h, --help                  Print this help and exit
@@ -28,7 +28,7 @@ Available options:
 -a, --all                   Install all the tools listed above
 -c, --vscode                Install vscode
 -z, --omz                  Install zsh and oh-my-zsh
--u, --zsh-user              Specify the user to enable zsh
+-u, --user                  Specify the user to install components
 -k, --pl10k                 Install powerlevel10k template on zsh
 -p, --pyenv                 Install pyenv
 -d, --docker                Install docker and docker compose
@@ -36,10 +36,10 @@ Available options:
 -s, --stable-debian-version Specify debian stable version to install the right versions of the component (eg: bookworm)
 
 Example:
-    - ./playbook-kali.sh --all --stable-debian-version bookworm --zsh-user kali
-    - ./playbook-kali.sh --omz --pl10k --zsh-user kali
+    - ./playbook-kali.sh --all --stable-debian-version bookworm --user kali
+    - ./playbook-kali.sh --omz --pl10k --user kali
     - ./playbook-kali.sh --pyenv
-    - ./playbook-kali.sh --docker --stable-debian-version bookworm
+    - ./playbook-kali.sh --docker --stable-debian-version bookworm --user kali
     - ./playbook-kali.sh --pyenv --omz --pl10k
 EOF
 exit
@@ -92,10 +92,6 @@ parse_params() {
 
         # zsh
         -z | --omz) zsh=1 ;;
-        # named parameter
-        -u | --zsh-user) 
-        user="${2-}"
-        shift ;;
 
         -k | --pl10k) pl10k=1 ;;
         -p | --pyenv) pyenv=1 ;;
@@ -103,9 +99,13 @@ parse_params() {
         
         # docker
         -d | --docker) docker=1 ;;
-        # named parameter
+
+        # named parameters
         -s | --stable-debian-version) 
         stable_debian_version="${2-}"
+        shift ;;
+        -u | --user) 
+        user="${2-}"
         shift ;;
 
         -?*) die "Unknown option: $1" ;;
@@ -189,7 +189,7 @@ install_pl10k(){
     # check if .oh-my-zsh is installed
     if [ ! -d "/home/${user}/.oh-my-zsh" ];
     then
-        die "${RED} Install oh-my-zsh first (./playbook-kali.sh --omz --zsh-user <username>)"
+        die "${RED} Install oh-my-zsh first (./playbook-kali.sh --omz --user <username>)"
     fi
 
     msg "\n${CYAN}******** Installing powerlevel10k ******"
@@ -234,15 +234,13 @@ fi\n"
     msg "${CYAN}****************************************************"
 }
 
-# ./Desktop/playbook-kali.sh --omz --pl10k --zsh-user kali --pyenv
-# ./Desktop/playbook-kali.sh --pyenv --zsh-user kali
 install_pyenv(){
     msg "\n${PURPLE}******** Installing pyenv ******"
 
     # Install pyenv
     curl -fsSL https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash &> /dev/null
 
-    # Load pyenv automatically by appendind to .zshrc
+    # Load pyenv automatically by appending to .zshrc
     msg "${PURPLE}Adding 'pyenv' to the load path."
     echo -e "\n# ======= pyenv load path config =======
 export PYENV_ROOT=\"\$HOME/.pyenv\"
@@ -259,7 +257,7 @@ enable_virtual_enviroment_on_bash(){
     # if powerlevel10k is present, enable virtualenv version on bash
     if [ ! -f ~/.p10k.zsh ];
     then
-        die "${RED} Install oh-my-zsh and powerlevel10k theme first (./playbook-kali.sh --omz --pl10k --zsh-user <username>)"
+        die "${RED} Install oh-my-zsh and powerlevel10k theme first (./playbook-kali.sh --omz --pl10k --user <username>)"
     fi
 
     if grep -q "ZSH_THEME=\"powerlevel10k\/powerlevel10k\"" /home/"${user}"/.p10k.zsh;
@@ -270,39 +268,50 @@ enable_virtual_enviroment_on_bash(){
 
 install_docker(){
     msg "\n${ORANGE}******** Installing docker ******"
-    # # Run the following command to uninstall all conflicting packages:
-    # for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+    # Run the following command to uninstall all conflicting packages:
+    set +Eeuo pipefail
+    for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt remove &> /dev/null $pkg; done
+    set -Eeuo pipefail
 
-    # # docker-ce can be installed from Docker repository. One thing to bear in mind, Kali Linux is based on Debian, so we need to use Debian’s current stable version (even though Kali Linux is a rolling distribution). At the time of writing (Oct. 2023), its "bookworm" (https://www.debian.org/releases/stable/index.en.html)
+    # docker-ce can be installed from Docker repository. One thing to bear in mind, Kali Linux is based on Debian, so we need to use Debian’s current stable version (even though Kali Linux is a rolling distribution). 
+    # At the time of writing (Oct. 2023), its "bookworm" (https://www.debian.org/releases/stable/index.en.html)
 
-    # #     ** Attenzione alla versione di debian **
-    # printf '%s\n' "deb https://download.docker.com/linux/debian bookworm stable" | sudo tee /etc/apt/sources.list.d/docker-ce.list
-    # curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-ce-archive-keyring.gpg
-    # sudo apt update
-    # sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    # *** pay attention about debian version
+    printf '%s\n' "deb https://download.docker.com/linux/debian $stable_debian_version stable" | sudo tee /etc/apt/sources.list.d/docker-ce.list &> /dev/null
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-ce-archive-keyring.gpg &> /dev/null
+    msg "\n${ORANGE} Updating system...."
+    sudo apt update &> /dev/null
 
-    # # add yourself to the docker group to use docker without sudo:
-    # sudo usermod -aG docker $USER
+    msg "\n${ORANGE} Installing docker...."
+    sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin &> /dev/null
+
+    # add yourself to the docker group to use docker without sudo:
+    sudo usermod -aG docker "$user"
     msg "${ORANGE}****************************************************\n"
 }
 
+# ./Desktop/playbook-kali.sh --omz --pl10k --pyenv --user kali
+# ./Desktop/playbook-kali.sh --golang
 install_golang(){ 
     msg "\n${YELLOW}******** Installing golang ******"
+
+    # Download golang
+
     # Remove any previous Go installation by deleting the /usr/local/go folder (if it exists), then extract the archive you just downloaded into /usr/local, creating a fresh Go tree in /usr/local/go:
-        # $ rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.3.linux-amd64.tar.gz
+    rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.3.linux-amd64.tar.gz
 
     # (You may need to run the command as root or through sudo).
-    #  Do not untar the archive into an existing /usr/local/go tree. This is known to produce broken Go installations.
+    # Do not untar the archive into an existing /usr/local/go tree. This is known to produce broken Go installations.
 
     # Add /usr/local/go/bin to the PATH environment variable.
     # You can do this by adding the following line to your $HOME/.profile or /etc/profile (for a system-wide installation):
 
-    # export PATH=$PATH:/usr/local/go/bin
+    export PATH=$PATH:/usr/local/go/bin
 
     # Note: Changes made to a profile file may not apply until the next time you log into your computer. To apply the changes immediately, just run the shell commands directly or execute them from the profile using a command such as source $HOME/.profile.
 
     # Verify that you've installed Go by opening a command prompt and typing the following command:
-    # $ go version
+    go version
 
     # Confirm that the command prints the installed version of Go.
     msg "${YELLOW}****************************************************\n"
@@ -326,12 +335,12 @@ then
     usage
 fi
 
-# check if the all flag is set
+# ==== all ====
 if [ $all -eq 1 ]  &&  { [ -z "$stable_debian_version" ] || [ -z "$user" ]; } ;
 then
     msg "${RED}It is not possible to proceed. Specify
     - stable debian version used in this moment (eg: bookworm)
-    - the user to enable zsh (eg: kali)"
+    - the user to enable components (eg: kali)"
     usage
 elif [ $all -eq 1 ] &&  { [ ! -z "$stable_debian_version" ] || [ ! -z "$user" ]; } ;
 then
@@ -374,10 +383,17 @@ then
     install_pyenv
 fi
 
-# if [ $docker -eq 1 ];
-# then
-#     install_docker
-# fi
+# === docker ===
+if [ $docker -eq 1 ] &&  { [ -z "$stable_debian_version" ] || [ -z "$user" ]; } ;
+then
+    msg "${RED}It is not possible to proceed. Specify
+    - stable debian version used in this moment (eg: bookworm)
+    - the user to enable components (eg: kali)"
+    usage
+elif [ $docker -eq 1 ] &&  { [ ! -z "$stable_debian_version" ] || [ ! -z "$user" ]; } ;
+then
+    install_docker
+fi
 
 # if [ $golang -eq 1 ];
 # then
