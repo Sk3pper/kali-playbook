59dfc2a6bb2510fd5be2a608d4192b79d2b0b6af
Sk3pper <13051136+Sk3pper@users.noreply.github.com>
2024-02-02 15:00:24 +0100

    change script name

--- Bash/kali-playbook.sh
+++ Bash/kali-playbook.sh
@@ -174,7 +174,7 @@ install_vscode(){
     notify_elevate "$cmd"
     sudo $cmd &>> ${log_path_file}
 
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg -v --dearmor > packages.microsoft.gpg
 
     sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg &>> "${log_path_file}"
     sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list' &>> "${log_path_file}"
